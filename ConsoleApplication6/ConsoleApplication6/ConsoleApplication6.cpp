da6c68ed6396eb592d4e161401aba37abf8f0ab8
Makkintoshia <85276507+Makkintoshia@users.noreply.github.com>
2021-06-06 09:54:07 +0300

    Add files via upload

---
+++ ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+const double pi = 3.14159265;
+const double e = 2.71828;
+main()
+{
+	double a;
+	double b;
+	double x;
+	double z;
+
+
+	//Ввод переменных с консоли
+
+	cout << "Write a";
+	cin >> a;
+	cout << "Write b";
+	cin >> b;
+	cout << "Write x";
+	cin >> x;
+
+	//Вычесление z
+
+	if (x <= a)
+	{
+		z = (1 + x * x) / (2 - 11 * sin(x)) + pow(e, sinh(x));
+	}
+
+	else if (x > a && x < b)
+	{
+		z = sqrt(1.57 - pow(x, 3) * pow(sin(x), 2)) + 4.1 * pow(e, (2 * x));
+	}
+
+	else (x >= b)
+	{
+		z = pow((asin(x) + acos(x) + log(x)), tan(x));
+	}
+	
+	cout << "z = " << z;
+	return 0;
+}
+
+
